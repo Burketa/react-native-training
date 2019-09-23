da435f65c51c8273de2fe41aa02e2183474bf392
burca <burca@burca.dev>
2019-09-23 10:43:03 -0300

    estilizacao container

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.textGreen}>burca passou por aqui muahaha</Text>
       <Text style={styles.textBlue}>burca passou por aqui muahaha</Text>
       <Text style={styles.textRed}>burca passou por aqui muahaha</Text>
+      <View style={styles.box} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  body: {
-    backgroundColor: Colors.blue,
+  container: {
+    alignItems: 'center',
   },
   textGreen: {
     color: '#0F0',
@@ -25,6 +24,11 @@ const styles = StyleSheet.create({
   },
   textRed: {
     color: '#F00',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#F00',
   },
 });
 
