6948d780946671fbb3cf69e747a8ed53dfbf8236
burca <burca@burca.dev>
2019-09-23 11:21:12 -0300

    adicionado axis, fazendo requisicao a api com async await

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component {
   static navigationOptions = {
     title: 'Le App',
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const response = await api.get('/products');
+
+    const {docs} = response.data;
+
+    console.log(docs);
   };
 
   render() {
