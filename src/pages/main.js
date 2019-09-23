44742e7b2e581babfb06ca91b4110f79409d2a3a
burca <burca@burca.dev>
2019-09-23 14:56:46 -0300

    adicionado estilizacoes

--- src/pages/main.js
+++ src/pages/main.js
@@ -33,15 +33,15 @@ export default class Main extends Component {
   };
 
   renderItem = ({item}) => (
-    <View>
-      <Text>{item.title}</Text>
-      <Text>{item.description}</Text>
-      <TouchableOpacity onPress={() => {}}>
-        <Text>Acessar</Text>
+    <View style={styles.productContainer}>
+      <Text style={styles.productTitle}>{item.title}</Text>
+      <Text style={styles.productDescription}>{item.description}</Text>
+      <TouchableOpacity style={styles.productButton} onPress={() => {}}>
+        <Text style={styles.productButtonText}>Acessar</Text>
       </TouchableOpacity>
-      <Button title="botao" onPress={() => {}}>
+      {/*<Button title="botao" onPress={() => {}}>
         <Text>Acessar</Text>
-      </Button>
+  </Button>*/}
     </View>
   );
 
@@ -54,6 +54,7 @@ export default class Main extends Component {
         <View style={styles.box} />
 
         <FlatList
+          contentContainerStyle={styles.list}
           data={this.state.docs}
           keyExtractor={item => item._id}
           renderItem={this.renderItem}
@@ -65,20 +66,69 @@ export default class Main extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     alignItems: 'center',
+    backgroundColor: '#FAFAFA',
   },
+
   textGreen: {
     color: '#0F0',
   },
+
   textBlue: {
     color: '#00F',
   },
+
   textRed: {
     color: '#F00',
   },
+
   box: {
     width: 60,
     height: 60,
     backgroundColor: '#F00',
+  },
+
+  list: {
+    padding: 20,
+  },
+
+  productContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+  },
+
+  productTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+
+  productDescription: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 24,
+  },
+
+  productButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#DA552F',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+
+  productButtonText: {
+    fontSize: 16,
+    color: '#DA552F',
+    fontWeight: 'bold',
   },
 });
